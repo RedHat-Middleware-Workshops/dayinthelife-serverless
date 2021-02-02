cc93e7b01596b95765be7ebad0763cf921769af7
christina Lin <christina@christinas-iMac.lan>
2021-02-02 18:23:11 -0500

    lab one first draft

---
+++ projects/lab-01/MeterConsumer.java
@@ -0,0 +1,32 @@
+// camel-k: language=java open-api=openapi-spec.yaml dependency=camel-openapi-java dependency=mvn:org.postgresql:postgresql:42.2.10 property-file=meters.properties secret=pg-login
+
+
+
+import org.apache.camel.builder.RouteBuilder;
+
+
+public class MeterConsumer extends RouteBuilder {
+
+    
+    @Override
+    public void configure() throws Exception {
+
+        from("direct:create").routeId("MetersFromAPI")
+            .throttle(3).timePeriodMillis(20000)
+            .unmarshal().json()
+            .setHeader("meterId",simple("${body.[value][meterId]}"))
+            .setHeader("status",simple("${body.[value][status]}"))
+            .setBody(simple("INSERT INTO meter_update(meter_id, timestamp, status_text) VALUES ('${headers.meterId}', to_timestamp(${body.[value][timestamp]}), '${headers.status}');"))
+        .log("SQL INSERT statement: ${body}")
+            .to("jdbc:dataSource")
+            .setBody(simple("SELECT address, id as meter_id, '${headers.status}' as status_text , latitude, longitude FROM meter where id = '${headers.meterId}' ;"))
+        .log("SQL SELECT statement: ${body}")
+            .to("jdbc:dataSource")
+            .marshal().json()
+        .to("kafka:{{consumer.topic}}?brokers={{kafka.host}}:{{kafka.port}}")
+        ;
+
+
+        
+    }
+}
