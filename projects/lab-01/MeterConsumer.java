52f03e27df993126aca034a709599a9a09cc11a2
Evan Shortiss <evanshortiss@gmail.com>
2021-08-25 12:55:45 +0100

    update lab 02 and verify steps

--- projects/lab-01/MeterConsumer.java
+++ projects/lab-01/MeterConsumer.java
@@ -1,13 +1,11 @@
-// camel-k: language=java open-api=openapi-spec.yaml dependency=camel-openapi-java dependency=mvn:org.postgresql:postgresql:42.2.10 property-file=meters.properties secret=pg-login
-
-
+// camel-k: language=java open-api=openapi-spec.yaml dependency=camel-openapi-java dependency=mvn:org.postgresql:postgresql:42.2.10 property-file=meters.properties secret=pg-login secret=rh-cloud-services-service-account
 
 import org.apache.camel.builder.RouteBuilder;
 
 
 public class MeterConsumer extends RouteBuilder {
 
-    
+
     @Override
     public void configure() throws Exception {
 
@@ -17,16 +15,15 @@ public class MeterConsumer extends RouteBuilder {
             .setHeader("meterId",simple("${body.[value][meterId]}"))
             .setHeader("status",simple("${body.[value][status]}"))
             .setBody(simple("INSERT INTO meter_update(meter_id, timestamp, status_text) VALUES ('${headers.meterId}', to_timestamp(${body.[value][timestamp]}), '${headers.status}');"))
-        .log("SQL INSERT statement: ${body}")
+            .log("SQL INSERT statement: ${body}")
             .to("jdbc:dataSource")
             .setBody(simple("SELECT address, id as meter_id, '${headers.status}' as status_text , latitude, longitude FROM meter where id = '${headers.meterId}' ;"))
-        .log("SQL SELECT statement: ${body}")
+            .log("SQL SELECT statement: ${body}")
             .to("jdbc:dataSource")
             .marshal().json()
-        .to("kafka:{{consumer.topic}}?brokers={{kafka.host}}:{{kafka.port}}")
-        ;
+            .to("kafka:{{producer.topic}}");
 
 
-        
+
     }
 }
